905aef7e3afaa5ee9f74efb9a2f7b1c1de991bd4
Ryan Kelley <rykelley@gmail.com>
2015-03-23 17:57:17 -0600

    PB project start

---
+++ Pirate Bartender/PirateBartender.py
@@ -0,0 +1,23 @@
+__author__ = 'Ryan'
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+}
+
+def user_input():
+    if input == (questions[0:]):
+        print("Do ye like yer drinks strong?")
+    elif input == (questions[1:]):
+        print("Do ye like it with a salty tang?")
